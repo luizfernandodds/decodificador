3a7d20405b6e3803c2dfcf7912f11bbad238e88e
Luiz Fernando Duarte <102691243+luizfernandodds@users.noreply.github.com>
2022-05-09 10:05:19 -0300

    Atualização das funções
    Adicionado as funções dos botões no Javascript
    
    - Função encriptar
    - Função descriptar
    - Botão Copy

--- js/script.js
+++ js/script.js
@@ -1,1 +1,46 @@
-document.getElementById("#mensagem1").style.display = "none";
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+let copyText= document.querySelector(".copy-text");
+
+function copy() {
+    let copyText = document.querySelector(".mensagem");
+    copyText.select();
+    document.execCommand("copy");
+  }
+
+function btnEncriptar(){
+    textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado;
+    mensagem.style.backgroundImage="none";
+}
+
+function btnDesencriptar(){
+    textoDesencriptado = desencriptar(inputTexto.value);
+    mensagem.value = textoDesencriptado;
+    mensagem.style.backgroundImage="none";
+}
+
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["a", "ai"],["e","enter"], ["i","imes"], ["o","ober"], ["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["ai", "a"],["enter","e"], ["imes","i"], ["ober","o"], ["ufat","u"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i=0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][0])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1]);
+        }
+    }
+    return stringDesencriptada;
+}
